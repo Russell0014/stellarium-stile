4fb67951f0d850a6eaf5049f5254f7168e95663e
Lachlan <lachlangoebel@gmail.com>
2025-03-26 14:51:56 +1100

    SCRUM-31 re-fixed typings

--- src/types/stellarium.d.ts
+++ src/types/stellarium.d.ts
@@ -190,29 +190,5 @@ function StelWebEngine(options: {
     translateFn?: (domain: string, str: string) => string;
 }): Promise<StellariumEngine>;
 
-export interface StellariumEngine {
-    core: {
-        observer: {
-            longitude: number;
-            latitude: number;
-            elevation: number;
-            utc: number;
-        };
-        fov: number;
-        selection: any;
-        atmosphere: {
-            visible: boolean;
-        };
-        landscapes: {
-            current: string;
-            visible: boolean;
-        };
-    };
-    calendar: any;
-    listLandscapes: () => string[];
-    getViewportSize: () => [number, number];
-    setViewportSize: (width: number, height: number) => void;
-    // Add other methods as you discover them
-}
 
 export {};
