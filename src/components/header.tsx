b0ee3a4ac4ba94c593039439fb282f99a66a9522
Lachlan Goebel <lachlangoebel@gmail.com>
2025-04-16 10:46:10 +1000

    Revert "Scrum 7 Add Date Functionality"

---
+++ src/components/header.tsx
@@ -0,0 +1,26 @@
+import styled from 'styled-components';
+import SearchBarController from './searchBarController';
+
+export default function Header() {
+	return (
+		<HeaderStyle>
+			<div> </div>
+			<SearchBarController />
+			<div> </div>
+		</HeaderStyle>
+	);
+}
+
+const HeaderStyle = styled.div`
+	background: rgb(255, 255, 255, 0.1);
+	position: absolute;
+	display: flex;
+	justify-content: space-between;
+	align-items: center;
+	width: 100vw;
+	height: 40px;
+	border-width: 2px;
+	border-height: 5px;
+	border-radius: 5;
+	z-index: 1;
+`;
