7ad018e12371cb02f94e5133b42d63cbd66e821f
Russell <Russells0014@gmail.com>
2025-05-07 12:18:29 +1000

    changed toggle buttons ui

--- src/components/ToggleControls.tsx
+++ src/components/ToggleControls.tsx
@@ -2,27 +2,19 @@ import styled from 'styled-components';
 import { useSEngine } from '@/context/SEngineContext';
 import swh from '@/assets/sw_helper';
 import { useState } from 'react';
+import constellationSVG from '@/assets/icons/constellation.svg';
+import star from '@/assets/icons/star.svg';
+import check from '@/assets/icons/check.svg';
+import uncheck from '@/assets/icons/uncheck.svg';
 
 export default function ToggleControls() {
 	const { engine } = useSEngine();
-	const [atmosphereVisible, setAtmosphereVisible] = useState(true);
 	const [starsVisible, setStarsVisible] = useState(true);
-	const [landscapeVisible, setLandscapeVisible] = useState(true);
 	const [constellationsVisible, setConstellationsVisible] = useState(true);
-
-	const toggleAtmosphere = () => {
-		setAtmosphereVisible(!atmosphereVisible);
-		swh.toggleAtmosphere(engine, !atmosphereVisible);
-	};
 
 	const toggleStars = () => {
 		setStarsVisible(!starsVisible);
 		swh.toggleStars(engine, !starsVisible);
-	};
-
-	const toggleLandscape = () => {
-		setLandscapeVisible(!landscapeVisible);
-		swh.toggleLandscapeVisibility(engine, !landscapeVisible);
 	};
 
 	const toggleConstellations = () => {
@@ -32,49 +24,57 @@ export default function ToggleControls() {
 
 	return (
 		<ControlsContainer>
-			<ButtonContainer>
-				<ToggleButton
-					onClick={toggleAtmosphere}
-					$active={atmosphereVisible}>
-					Atmosphere
-				</ToggleButton>
-				<ToggleButton
-					onClick={toggleStars}
-					$active={starsVisible}>
-					Stars
-				</ToggleButton>
-				<ToggleButton
-					onClick={toggleLandscape}
-					$active={landscapeVisible}>
-					Landscape
-				</ToggleButton>
-				<ToggleButton
-					onClick={toggleConstellations}
-					$active={constellationsVisible}>
-					Constellations
-				</ToggleButton>
-			</ButtonContainer>
+			<ToggleButton
+				onClick={toggleConstellations}
+				$active={constellationsVisible}>
+				<img
+					src={constellationSVG}
+					alt='constellation icon'
+				/>
+				Constellations
+				<StatusIcon
+					src={constellationsVisible ? check : uncheck}
+					alt=''
+				/>
+			</ToggleButton>
+			<ToggleButton
+				onClick={toggleStars}
+				$active={starsVisible}>
+				<img
+					src={star}
+					alt='star icon'
+				/>
+				Stars
+				<StatusIcon
+					src={starsVisible ? check : uncheck}
+					alt=''
+				/>
+			</ToggleButton>
 		</ControlsContainer>
 	);
 }
 
 const ControlsContainer = styled.div`
 	position: absolute;
-	background: rgba(255, 255, 255, 0.1);
+	display: flex;
+	flex-direction: column;
+	gap: 10px;
 	padding: 14px;
 	border-radius: 5px;
+	border: 10px;
 	left: 20px;
 	bottom: 60px;
 	z-index: 1;
 `;
 
-const ButtonContainer = styled.div`
-	display: flex;
-	gap: 10px;
-`;
-
 const ToggleButton = styled.button<{ $active: boolean }>`
-	background: ${(props) => (props.$active ? 'rgba(0, 203, 80, 0.2)' : 'rgba(255, 255, 255, 0.1)')};
+	display: flex;
+	align-items: center;
+	gap: 10px;
+
+	background: rgba(0, 0, 0, 0.5);
+	/*background: ${(props) =>
+		props.$active ? 'rgba(0, 203, 80, 0.2)' : 'rgba(255, 255, 255, 0.1)'};*/
 	color: white;
 	border: 1px solid rgba(255, 255, 255, 0.3);
 	padding: 5px 15px;
@@ -89,4 +89,9 @@ const ToggleButton = styled.button<{ $active: boolean }>`
 	&:active {
 		background: rgba(0, 203, 80, 0.4);
 	}
+`;
+
+const StatusIcon = styled.img`
+	margin-left: auto;
+	padding
 `;
