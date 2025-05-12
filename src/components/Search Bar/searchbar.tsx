d9c9e79602aa7a3b259693399c6fdd303677275b
MortalFlame21 <markgio2100@gmail.com>
2025-05-12 20:39:59 +1000

    Edit searchbar.tsx. Edit search result dropdown.

--- src/components/Search Bar/searchbar.tsx
+++ src/components/Search Bar/searchbar.tsx
@@ -1,6 +1,9 @@
 import styled from 'styled-components';
 import type { SearchResults, SearchResult } from '../../types/stellarium';
 
+// temp for now
+import icon_search from '@/assets/icons/search.svg';
+
 type Props = {
 	search: string;
 	results: SearchResults;
@@ -27,6 +30,7 @@ export default function SearchBar({ search, results, onSearch, onClose, onResult
 							key={searchresult.match}
 							onMouseDown={(e) => e.preventDefault()}
 							onClick={() => onResultClick(searchresult)}>
+							<img src={icon_search} />
 							{searchresult.short_name}
 						</SearchResultItem>
 					))}
@@ -41,21 +45,21 @@ const SearchDiv = styled.div`
 	flex-direction: column;
 	align-self: start;
 	position: relative;
-	width: 300px;
+	width: 350px;
 `;
 
 const SearchText = styled.input`
 	id: search;
-	background: rgba(0, 0, 0, 0.7);
+	background: rgba(0, 0, 0, 0.6);
+	backdrop-filter: blur(5px);
 	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
 	border: 1px solid rgba(255, 255, 255, 0.3);
-	border-radius: 0.33rem;
+	border-radius: 8px;
 	color: white;
 	padding: 0.5rem 1.25rem;
 
 	&:focus {
 		outline: none;
-		border-color: rgba(255, 255, 255);
 	}
 
 	&::placeholder {
@@ -68,8 +72,8 @@ const SearchDropDown = styled.div`
 	flex-direction: column;
 	background: rgba(0, 0, 0, 0.6);
 	backdrop-filter: blur(5px);
-	margin-top: 5px;
-	border-radius: 4px;
+	margin-top: 0.5rem;
+	border-radius: 8px;
 	width: 100%;
 	max-height: 300px;
 	overflow-y: auto;
@@ -77,10 +81,14 @@ const SearchDropDown = styled.div`
 	top: 100%;
 	z-index: 10;
 	box-shadow: 0 4px 8px rgba(0, 0, 0, 0.3);
+	border: 1px solid rgba(255, 255, 255, 0.3);
 `;
 
 const SearchResultItem = styled.div`
-	padding: 8px 12px;
+	display: flex;
+	align-items: center;
+	gap: 0.5rem;
+	padding: 1.25rem 1rem;
 	cursor: pointer;
 	transition: background-color 0.2s;
 	color: white;
