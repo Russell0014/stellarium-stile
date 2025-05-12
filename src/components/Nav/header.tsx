bac09ad96c3ad7c148670d7760eedeaa84417a65
MortalFlame21 <markgio2100@gmail.com>
2025-05-12 19:56:59 +1000

    Edit header.tsx. Cleanup.

--- src/components/Nav/header.tsx
+++ src/components/Nav/header.tsx
@@ -4,23 +4,18 @@ import SearchBarController from '../Search Bar/searchBarController';
 export default function Header() {
 	return (
 		<HeaderStyle>
-			<div> </div>
 			<SearchBarController />
-			<div> </div>
 		</HeaderStyle>
 	);
 }
 
 const HeaderStyle = styled.div`
-	background: rgb(255, 255, 255, 0.1);
-	position: absolute;
-	display: flex;
-	justify-content: space-between;
-	align-items: center;
-	width: 100vw;
-	height: 40px;
-	border-width: 2px;
-	border-height: 5px;
-	border-radius: 5;
 	z-index: 1;
+	position: absolute;
+	top: 1rem;
+	left: 0;
+	right: 0;
+	display: flex;
+	justify-content: center;
+	align-items: center;
 `;
