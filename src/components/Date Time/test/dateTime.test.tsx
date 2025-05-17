13db440ffe5c166047bdc0dc005523a599adc965
MortalFlame21 <markgio2100@gmail.com>
2025-05-17 22:13:53 +1000

    Complete DateTime tests. maybe. ?

--- src/components/Date Time/test/dateTime.test.tsx
+++ src/components/Date Time/test/dateTime.test.tsx
@@ -28,31 +28,76 @@ describe('DateTime', () => {
 		expect(fragment).not.null;
 	});
 
-	it('Buttons increments time on press.', () => {
-		let moment = moment_;
-		const changeDateTime = (s: string, n: number) => {
-			moment.add(n, s as moment.DurationInputArg2);
-		};
+	describe('Button', () => {
+		it('Increments time on click', () => {
+			let mnt = moment_;
+			const changeDateTime = (s: string, n: number) => {
+				mnt.add(n, s as moment.DurationInputArg2);
+			};
 
-		// we will simulate button presses here
-		changeDateTime('h', 1);
-		changeDateTime('h', 1);
+			// we will simulate button presses here
+			changeDateTime('h', 1);
+			changeDateTime('h', 1);
 
-		const { asFragment } = render(
-			<SelectionButton
-				moment={moment}
-				changeDateTime={changeDateTime}
-			/>,
-		);
-		// div/div/date_time
-		const time = asFragment().lastChild?.lastChild?.textContent;
+			const { asFragment } = render(
+				<SelectionButton
+					moment={mnt}
+					changeDateTime={changeDateTime}
+				/>,
+			);
+			// div/div/date_time
+			const time = asFragment().lastChild?.lastChild?.textContent;
 
-		// just compare formatted dates
-		expect(asFragment());
-		expect(time).toEqual(`${moment.format('HH')}:${moment.format('mm')}`);
+			// just compare formatted dates
+			expect(asFragment());
+			expect(time).toEqual(`${mnt.format('HH')}:${mnt.format('mm')}`);
+		});
+
+		it('Incrementing time and decrementing is original value', () => {
+			let mnt = moment_;
+			const changeDateTime = (s: string, n: number) => {
+				mnt.add(n, s as moment.DurationInputArg2);
+			};
+
+			changeDateTime('h', 1);
+			changeDateTime('h', -1);
+
+			const { asFragment } = render(
+				<SelectionButton
+					moment={mnt}
+					changeDateTime={changeDateTime}
+				/>,
+			);
+			const time = asFragment().lastChild?.lastChild?.textContent;
+
+			expect(asFragment());
+			expect(time).toEqual(`${mnt.format('HH')}:${mnt.format('mm')}`);
+		});
 	});
 
-	it('Time Changes.', () => {});
+	it('Reset button resets time.', () => {
+		// same as other test, we will simulate the reset.
+		let mnt = moment_.add(15, 'h');
 
-	it('Reset button.', () => {});
+		const resetTime = () => {
+			mnt = moment(new Date());
+		};
+
+		resetTime();
+
+		const { asFragment } = render(
+			<DateTime
+				DateSlider={slider}
+				resetTime={resetTime}
+				moment={mnt}
+				changeDateTime={(s: string, n: number) => {}}
+			/>,
+		);
+
+		const TimeUpdateComponent = asFragment().lastChild?.lastChild?.childNodes[1];
+		const time = TimeUpdateComponent?.lastChild?.textContent;
+
+		expect(asFragment());
+		expect(time).toEqual(`${mnt.format('HH')}:${mnt.format('mm')}`);
+	});
 });
