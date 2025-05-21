f7b6ac3b9447eeb2ba1f88af06feb0fa13f7e6f4
Lachlan <lachlangoebel@gmail.com>
2025-05-21 10:57:51 +1000

    Added code comments for searchBarController

--- src/components/Search Bar/searchBarController.tsx
+++ src/components/Search Bar/searchBarController.tsx
@@ -5,14 +5,29 @@ import SkyObjectInfoPopup from '../SkyObjectInfoPopup';
 import type { SearchResults, SearchResult } from '../../types/stellarium';
 import { useSEngine } from '../../context/SEngineContext';
 
-export default function SearchBarController() {
-	const [search, setSearch] = useState<string>('');
-	const [results, setResults] = useState<SearchResults>([]);
-	const [debouncedSearch, setDebouncedSearch] = useState<string>(search);
-	const [selectedObject, setSelectedObject] = useState<SearchResult | null>(null);
-	const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false);
-	const { engine } = useSEngine();
+/**
+ * @file SearchBarController.tsx - Controller component for handling search functionality and displaying sky object information
+ * @module SearchBarController
+ */
 
+/**
+ * SearchBarController component
+ * Manages search input, fetches search results for Stellarium engine,
+ * handles result selection, and displays an information popup for the selected sky object
+ * @returns {JSX.Element}
+ */
+export default function SearchBarController() {
+	const [search, setSearch] = useState<string>(''); //State for storyin current search input value
+	const [results, setResults] = useState<SearchResults>([]); //State for storing Search Result
+	const [debouncedSearch, setDebouncedSearch] = useState<string>(search); // State for debounced search term.
+	const [selectedObject, setSelectedObject] = useState<SearchResult | null>(null); // State for currently selected sky object from search results
+	const [isPopupOpen, setIsPopupOpen] = useState<boolean>(false); //State to control visibility of sky object popup
+	const { engine } = useSEngine(); //Stellarium Engine Instance
+
+	/**
+	 * useEffect hook to debounce the search input
+	 * Updates `debouncedSearch` 500ms after the 'search' state changes
+	 */
 	useEffect(() => {
 		const handler = setTimeout(() => {
 			setDebouncedSearch(search);
@@ -23,7 +38,10 @@ export default function SearchBarController() {
 		};
 	}, [search]);
 
-	//Implement Timeout to wait for user to stop typing
+	/**
+	 * useEffect hook to fetch search results when `debouncedSearch` or the engine's sky culture changes.
+	 * Implements a timeout to wait for the user to stop typing before fetching
+	 */
 	useEffect(() => {
 		if (debouncedSearch) {
 			if (search === '' || search.length > 10) {
@@ -31,32 +49,56 @@ export default function SearchBarController() {
 			}
 
 			const fetchAllData = async () => {
+				//Fetches search data from the Stellarium Helper
 				try {
 					const results = await swh.searchObjects(search, 10, engine);
 					trim(results);
 					setResults(results);
 				} catch (error) {
+					//Need to update Error Handling for to throw specific error
 					throw new Error('Error fetching results: ' + error);
 				}
 			};
 
 			fetchAllData();
 		}
+		//Depedency array includes debouncedSearch and a property from the engine that might affect search results.
 	}, [debouncedSearch, engine?.core?.skycultures?.current_id]);
 
+	/**
+	 * Trims the "NAME " prefix from the 'match' property of the search results
+	 * This mutates the 'results' array directly.
+	 * @param {SearchResults} resultsToTrim - The array of Search Results to Trim
+	 * @returns {SarchResults} - Modified Array of Search Results
+	 */
 	const trim = (results: SearchResults): SearchResults => {
 		results.map((searchresult) => (searchresult.match = searchresult.match.replace('NAME ', '')));
 		return results;
 	};
 
+	/**
+	 * Handles changes in the search input field
+	 * @param {string} search - The new search string
+	 */
 	const handleChange = (search: string): void => {
 		setSearch(search);
 	};
 
+	/**
+	 * Handles the closing of search dropdown or cleaning of search
+	 * Resets the search input and results
+	 */
 	const onClose = (): void => {
 		setSearch('');
 		setResults([]);
 	};
+
+	/**
+	 * Handles clicking on a search result.
+	 * Extracts object information and determines object ID for Stellarium Engine
+	 * Navigates to the Object in the engine, and displays an info popup.
+	 * @param {SearchResult} result
+	 */
 
 	const handleResultClick = (result: SearchResult): void => {
 		// Extract unique identifier from the result
